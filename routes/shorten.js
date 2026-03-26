65887b4ba836c4cc5b081638cc7b875ac52ce14a
Ysweb3 <yssh88972@gmail.com>
2026-03-26 12:41:21 +0530

    add url redirect and validation

--- routes/shorten.js
+++ routes/shorten.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const {nanoid} = require('nanoid');
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient();
 
-router.post('/', (req, res) => {
-    console.log(req.body.longUrl);
-    res.send(req.body.longUrl);
+function isValidUrl(string){
+  try{
+    new URL(string);
+    return true;
+  }
+  catch(err){
+    return false;
+  }
+}
+
+
+
+router.post('/', async(req, res) => {
+    const code = nanoid(6);
+   
+    console.log("longUrl: " + req.body.longUrl);
+    if(isValidUrl(req.body.longUrl)){
+      const result = await prisma.url.create({
+        data: {
+        longUrl: req.body.longUrl,
+        shortCode: code,
+        }
+    })
+    console.log("code: " + code);
+    res.send(result);
+    }
+    else{
+      res.status(404).send('URL not valid');
+    }
 });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,42 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+require('dotenv/config')
 const express = require('express');
-const {nanoid} = require('nanoid');
-
 
 const app = express();
 
-const code = nanoid(6);
-console.log(code);
+
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 
-const shortUrlRouter = require('./routes/shorten');
-app.use('/shorten', shortUrlRouter);
-
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+
+const shortUrlRouter = require('./routes/shorten');
+app.use('/shorten', shortUrlRouter);
+
+app.get('/:code', async(req, res) => {
+    console.log(req.params.code);
+    const url = await prisma.url.findUnique({
+        where: {
+        shortCode: req.params.code
+    }
+  })
+  if(!url){
+    res.status(404).send('URL not found');
+    console.log('URL not found '+req.params.code);
+    
+  }
+  else{
+      res.redirect(url.longUrl);
+
+  }
+
+});
+
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
